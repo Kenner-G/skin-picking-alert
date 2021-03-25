7314b8cdee45cd1348d3001931574f4af32989e1
Kenner <k.enner2k19@gmail.com>
2021-03-25 14:27:05 -0600

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ handTrack.startVideo(video)
         if(status){
             navigator.getUserMedia({video:{}},stream => {
                 video.srcObject=stream;
-                setInterval(runDetect,1000);
+                setInterval(runDetect,300);
             },
             err => console.log(err)
             );
@@ -41,7 +41,7 @@ function runDetect()
 
 const modelParams = {
     flipHorizontal: false,   // flip e.g for video 
-    imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
+    imageScaleFactor: 0.2,  // reduce input image size for gains in speed.
     maxNumBoxes: 20,        // maximum number of boxes to detect
     iouThreshold: 0.5,      // ioU threshold for non-max suppression
     scoreThreshold: 0.81,    // confidence threshold for predictions.
