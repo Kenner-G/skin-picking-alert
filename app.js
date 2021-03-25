c5dde2b90e028450d1f51f4a9a6637699d081d0c
Kenner <k.enner2k19@gmail.com>
2021-03-25 14:24:42 -0600

    Update app.js

--- app.js
+++ app.js
@@ -41,9 +41,9 @@ function runDetect()
 
 const modelParams = {
     flipHorizontal: false,   // flip e.g for video 
-    imageScaleFactor: 0.5,  // reduce input image size for gains in speed.
+    imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
     maxNumBoxes: 20,        // maximum number of boxes to detect
-    iouThreshold: 0.7,      // ioU threshold for non-max suppression
+    iouThreshold: 0.5,      // ioU threshold for non-max suppression
     scoreThreshold: 0.81,    // confidence threshold for predictions.
   }
   
